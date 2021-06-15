72b6f8719fe4e1cdc436bac7f874d85065541c72
Johnatan Souza <jgs8115@gmail.com>
2021-06-14 22:55:38 -0300

    insert phone Router

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
 # send_me
 
 Devolva, muitas vezes é uma das poucas coisas que a pessoa tem!
+Mas vale 10 Iphones na mão do que 01 bloqueado!
 
 `` js
 phone:{
 brand:"",
 model:"",
-color:"",
+color:"",   
 details: "",
 "photos":""
 fair_shipp: ""
 }``
+
--- controllers/cellPhoneController.js
+++ controllers/cellPhoneController.js
@@ -2,24 +2,39 @@ const knex = require("../database");
 
 module.exports = {
   async getAllPhones(req, res) {
-    const results = await knex("cellphone").select("brand", "model", "color ");
-    return res.json(results);
+    try {
+      const results = await knex("cellphone").select(
+        "brand",
+        "model",
+        "photos",
+        "color ",
+        "photos"
+      );
+    } catch (error) {
+    } finally {
+      return res.json(results);
+    }
   },
 
   async insertPhone(req, res) {
-    const { brand, model, color, found_at, details, fair_shipp } = req.body;
-    await knex("cellphone").insert({
-      brand,
-      model,
-      color,
-      found_at,
-      details,
-      fair_shipp,
-    });
-    return res.status(201).json({
-      message: "Cell Phone successfully registered thanks for colaborate <3",
-      brand: brand,
-      color: color,
-    });
+    try {
+      const { brand, model, color, found_at, photos, details, fair_shipp } =
+        req.body;
+      await knex("cellphone").insert({
+        brand,
+        model,
+        color,
+        found_at,
+        details,
+        fair_shipp,
+      });
+      return res.status(201).json({
+        message: "Cell Phone successfully registered thanks for colaborate <3",
+        brand: brand,
+        color: color,
+      });
+    } catch (error) {
+      res.status(400).send(`Error: ${error}`);
+    }
   },
 };
